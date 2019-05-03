a77a197e4d749ccaf3d2f85ffa8f53f0b24ac02d
Alyyy <aly.m.abdelrahman@gmail.com>
2019-05-03 11:26:19 +0200

    edit DB commit

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
-const dbURL = "mongodb://localhost:27017/goodreads";
+const dbURL = "mongodb+srv://samar:samar123456_@cluster0-7toh7.mongodb.net/test?retryWrites=true";
 // "mongodb+srv://samar:samar123456_@cluster0-7toh7.mongodb.net/test?retryWrites=true";
 mongoose.connect(dbURL, { useNewUrlParser: true, autoIndex: true, useCreateIndex: true });
